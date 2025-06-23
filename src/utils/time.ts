d3d5919f7b2737470d046ee95fb8797eee533479
uy/sun <hmy0119@gmail.com>
2025-06-23 21:57:17 +0800

    fix formatTime

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,8 @@
-export const formatTime = (seconds: number): string => {
-  const mins = Math.floor(seconds / 60)
-  const secs = Math.floor(seconds % 60)
-  return `${mins}:${secs.toString().padStart(2, '0')}`
-}
+export const formatTime = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  const hours = date.getHours();
+  const mins = date.getMinutes();
+  return `${hours.toString().padStart(2, "0")}:${mins
+    .toString()
+    .padStart(2, "0")}`;
+};
